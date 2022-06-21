343c035a3da6c321217eb407ce5a4b6b0688d01b
MatheusFranzener <felipe_mielke-vieira@estudante.sc.senai.br>
2022-06-20 21:50:00 -0300

    20/06

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,10 @@
 const crud = require("./crud");
 
+async function buscarDados() {
+    const dados = await crud.get("pessoas");
+    console.log(dados);
+}
+
 async function buscarDadoId() {
     const dados = await crud.getById("pessoas", "cWCQGpRfSlBHHO3iFvxA");
     console.log(dados);
